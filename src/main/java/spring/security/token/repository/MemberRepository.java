bf96fe593226dd88ef1e20e39bc35d49d71640f3
JP-company <wjsdj2008@gmail.com>
2024-05-10 12:11:06 +0900

    [feat] 일반 회원 저장 서비스 메서드

--- src/main/java/spring/security/token/repository/MemberRepository.java
+++ src/main/java/spring/security/token/repository/MemberRepository.java
@@ -1,2 +1,7 @@
-package spring.security.token.repository;public interface MemberRepository {
+package spring.security.token.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.security.token.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/main/java/spring/security/token/service/MemberService.java
+++ src/main/java/spring/security/token/service/MemberService.java
@@ -1,2 +1,19 @@
-package spring.security.token.service;public class MemberService {
+package spring.security.token.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.security.token.domain.Member;
+import spring.security.token.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long save(String loginId, String loginPassword) {
+        Member member = Member.create(loginId, loginPassword);
+        Member savedMember = memberRepository.save(member);
+        return savedMember.getId();
+    }
 }
