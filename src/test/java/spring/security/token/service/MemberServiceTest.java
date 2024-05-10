2632b18a49c7442bb562630960581e1379c5c7d9
JP-company <wjsdj2008@gmail.com>
2024-05-10 12:13:05 +0900

    [test] 일반 회원 저장 테스트

--- src/test/java/spring/security/token/service/MemberServiceTest.java
+++ src/test/java/spring/security/token/service/MemberServiceTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package spring.security.token.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class MemberServiceTest {
-  
+
+    @Autowired MemberService memberService;
+
+
+    @DisplayName("일반 회원 저장 테스트")
+    @Test
+    void member_save_test() {
+        // given
+        String loginId = "wjsdj2009";
+        String loginPassword = "Qwer1234!";
+
+        // when, then
+        Long savedMemberId = memberService.save(loginId, loginPassword);
+
+        // then
+        assertThat(savedMemberId).isEqualTo(1L);
+    }
 }
