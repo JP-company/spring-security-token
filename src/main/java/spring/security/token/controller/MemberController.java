3ff476172f6f9bd6dfc737d99ee5862e43d36a3f
JP-company <wjsdj2008@gmail.com>
2024-05-10 13:11:20 +0900

    [feat] 전체 회원 목록 조회 API

--- src/main/java/spring/security/token/controller/MemberController.java
+++ src/main/java/spring/security/token/controller/MemberController.java
@@ -3,12 +3,17 @@ package spring.security.token.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import spring.security.token.controller.dto.request.MemberJoinForm;
 import spring.security.token.controller.dto.response.ResponseDto;
+import spring.security.token.controller.dto.response.ResponseListDto;
+import spring.security.token.domain.Member;
 import spring.security.token.service.MemberService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +29,16 @@ public class MemberController {
 
         ResponseDto<Long> responseBody = ResponseDto.<Long>builder()
                 .data(savedMemberId)
+                .build();
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping("members")
+    public ResponseEntity<ResponseListDto<Member>> getMembers() {
+        List<Member> members = memberService.findMembers();
+
+        ResponseListDto<Member> responseBody = ResponseListDto.<Member>builder()
+                .data(members)
                 .build();
         return ResponseEntity.ok(responseBody);
     }
